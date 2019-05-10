efd726da518a5e03b0b5cde24dbec94d08d0356c
gaoweiinter <gaoweiinter@hotmail.com>
2019-05-10 13:35:06 +0800

    Client 2

---
+++ EurekaClient2/src/main/java/com/eureka/client/demo/EurekaClientController.java
@@ -0,0 +1,20 @@
+package com.eureka.client.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EurekaClientController {
+	@Autowired
+	private DiscoveryClient discoveryClient;
+
+	@GetMapping("/client")
+	public String eurekaService() {
+		String services = "Services 8765: " + discoveryClient.getServices();
+		System.out.println(services);
+		return services;
+	}
+
+}
