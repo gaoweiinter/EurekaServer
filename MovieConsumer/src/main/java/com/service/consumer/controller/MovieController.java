c01c47d4751c2bc88b1259fe9337c635f7a0d9f5
gaoweiinter <gaoweiinter@hotmail.com>
2019-05-15 17:20:24 +0800

    MovieConsumer

---
+++ MovieConsumer/src/main/java/com/service/consumer/controller/MovieController.java
@@ -0,0 +1,61 @@
+package com.service.consumer.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.service.user.bean.User;
+
+@RestController
+public class MovieController {
+	@Value("${user.serviceUrl}")
+	private String serviceUrl;
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Autowired
+	private DiscoveryClient discoveryClient;
+
+	@Autowired
+	private LoadBalancerClient loadBalancerClient;
+
+	@GetMapping("/{id}")
+	public User findById(@PathVariable Long id) {
+		System.out.println("URL: " + serviceUrl);
+		return restTemplate.getForObject(this.serviceUrl + "user/" + id, User.class);
+	}
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public List<User> findAll() {
+		return restTemplate.getForObject(serviceUrl + "users", List.class);
+	}
+
+	@GetMapping("/user-instance")
+	public List<ServiceInstance> showInfo() {
+		return this.discoveryClient.getInstances("User");
+	}
+
+	@GetMapping("/services")
+	public List<String> showServiceInfo() {
+		return this.discoveryClient.getServices();
+	}
+
+	@GetMapping("/log-user-instance")
+	public ServiceInstance logUserInstance() {
+		ServiceInstance serviceInstance = this.loadBalancerClient.choose("EUREKA-CLIENT-A");
+		System.out.println("Service: " + serviceInstance.getInstanceId() + " Host: " + serviceInstance.getHost()
+				+ " port: " + serviceInstance.getPort());
+		return serviceInstance;
+	}
+}
---
+++ MovieConsumer/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user(id, username, name, age, balance) values( 1, 'account1', '张三', 20, 100.00);
+insert into user(id, username, name, age, balance) values( 2, 'account2', '李四', 28, 190.00);
+insert into user(id, username, name, age, balance) values( 3, 'account3', '王五', 34, 300.00);
